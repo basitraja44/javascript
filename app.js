ca34f41102a2b74fe963434eb2c03244b8bfc6ab
basitraja44 <br0857847@gmail.com>
2024-12-21 17:36:21 +0500

    calculatar

---
+++ app.js
@@ -0,0 +1,15 @@
+let num1 = "2"
+
+let ch2 = "2"
+
+
+console.log(num1+ch2)
+
+
+
+
+function calculate(){
+    let num2 = Number  (prompt("first"));
+    let num3 = Number  (prompt("second"));
+    alert(num2+num3)
+}
---
+++ scripit.js
@@ -0,0 +1,10 @@
+alert("hello world")
+console.log("hello world")
+document.body.style.backgroundColor="red"
+
+
+
+let num1 = Number (prompt ("first"));
+let num2 = Number (prompt ("second"));
+alert(num1+num2)
+
