71bea4f1942b9d26f825c1c79b34cc7c03ea14b5
basitraja44 <br0857847@gmail.com>
2024-12-31 17:04:20 +0500

    new commit

---
+++ if else/index.js
@@ -0,0 +1,11 @@
+let name = "karachi"
+let name2 = "karachi"
+
+
+if(name === name2){
+    console.log("yes we are neibhors")
+   
+}
+else{
+    console.log("no we are not neibhors")
+}
---
+++ weather suggesion/app.js
@@ -0,0 +1,11 @@
+let userInput = prompt("Write Your City Name Here.");
+
+if (userInput === "karachi") {
+  alert("take your jacket on");
+} else if (userInput === "hydrabad") {
+  alert("Avoid night outings");
+} else if (userInput === "islamabad") {
+  alert("Its too cold at night");
+} else {
+  alert("Sorry, We have not Data for your city");
+}
