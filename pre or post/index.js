1bd4fbeb6f30ad8049c6cbd1f9a40eab98e993c1
basitraja44 <br0857847@gmail.com>
2024-12-31 17:54:33 +0500

    new commit

---
+++ pre or post/index.js
@@ -0,0 +1,14 @@
+// Post Increament
+// num1 = 5;
+
+
+// console.log(num1++);
+// console.log(num1++);
+// console.log(num1);
+
+// pre increament
+num4 = 4;
+
+console.log(++num4);
+console.log(++num4);
+console.log(num4);
--- weather suggesion/app.js
+++ weather suggesion/app.js
@@ -1,11 +1,12 @@
-let userInput = prompt("Write Your City Name Here.");
+let userinput =prompt("write your city name here")
 
-if (userInput === "karachi") {
-  alert("take your jacket on");
-} else if (userInput === "hydrabad") {
-  alert("Avoid night outings");
-} else if (userInput === "islamabad") {
-  alert("Its too cold at night");
-} else {
-  alert("Sorry, We have not Data for your city");
+if(userinput === "karachi"){
+    alert("take your jacket on")
+}else if(userinput === "hydrabaad"){
+    alert("avoid night outing")
+}else if(userinput === "islamabad"){
+    alert("it very cold")
+}else{
+    alert("sorry i have no data")
 }
+   
