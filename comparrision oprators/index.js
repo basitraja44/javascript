deb926f4ec595eb2e18051800d4cc659117aa526
basitraja44 <br0857847@gmail.com>
2025-01-02 17:34:48 +0500

    new code

---
+++ comparrision oprators/index.js
@@ -0,0 +1,20 @@
+let rollnum = 123456
+let userrollnum = 123456
+
+if(rollnum === userrollnum){
+    console.log("you are pass")
+}else{
+    console.log("you are not pass")
+}
+
+
+
+let studentmarks = 80
+
+if(studentmarks >=90){
+    console.log("A+ grade")
+}else if(studentmarks >=80){
+    console.log("A grade")
+}else{
+    console.log("B grade")
+}
